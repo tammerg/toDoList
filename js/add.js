d624ab7b5b741d40c12b700020a465d78a7ebf7c
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-11-23 18:22:43 -0500

    trying to add checkbox to the add on click

--- js/add.js
+++ js/add.js
@@ -1,35 +1,39 @@
 $(document).ready(function (){ 
 
-   $(".btn-primary").on("click", function(e) {
-    e.preventDefault();
+ $(".btn-primary").on("click", function(e) {
+  e.preventDefault();
 
-    var newWord, newRow, wordTd, deleteButton, deleteTd;
-    var isDuplicate;
+  var newWord, newRow, wordTd, deleteButton, deleteTd;
+  var isDuplicate;
 
-    newWord = $(".newWord").val();
-    newTime = $(".newTime").val();  
+  newWord = $(".newWord").val();
+  newTime = $(".newTime").val(); 
+  newCheck = $(".newCheck").val(); 
 
-    $("td.word-td").each(function(){
-      if($(this).text() === newWord) {
-        isDuplicate = true;
-        return;
-      }
-    });
-
-    if(isDuplicate) {
-      $(".alert").fadeIn(1000)
+  $("td.word-td").each(function(){
+    if($(this).text() === newWord) {
+      isDuplicate = true;
       return;
     }
-
-    newRow = $("<tr>");
-    wordTd = $("<td>").addClass("word-td").append(newWord);
-    timeTd = $("<td>").addClass("word-td").append(newTime); 
-    deleteButton = $("<button>").addClass("btn btn-danger buttonRemove").append("Remove");
-    deleteTd = $("<td>").append(deleteButton);
-
-    newRow.append(wordTd).append(timeTd).append(deleteTd); 
-
-    $("tbody").append(newRow);
-    $("#newWord").val("")
-    });
   });
+
+  if(isDuplicate) {
+    $(".alert").fadeIn(1000)
+    return;
+  }
+
+  newRow = $("<tr>");
+  wordTd = $("<td>").addClass("word-td").append(newWord).before();
+  timeTd = $("<td>").addClass("word-td").append(newTime).before();
+  newCheck = $("<td>").addClass("word-td").append(newCheck).before(); 
+
+  deleteButton = $("<button>").addClass("btn btn-danger buttonRemove").append("Remove");
+  deleteTd = $("<td>").append(deleteButton);
+
+  newRow.append(wordTd).append(timeTd).append(newCheck).append(deleteTd).before(); 
+
+  $("tbody").append(newRow);
+  $("#newWord").val("")
+  });
+});
+
