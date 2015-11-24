226a2b094481c9d1ad075ed3189cec8d0a9b12ec
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-11-24 00:35:33 -0500

    added jquery that moved my added TD elements to the bottom panel body

--- js/remover.js
+++ js/remover.js
@@ -3,3 +3,10 @@ $(document).ready(function (){
     $(this).parents("tr").remove();
       });
   });
+$(document).ready(function (){
+  $(document).on("click",".btn-danger", function(){
+    $t = $(this).closest('tr').find('td')[0];
+    $(this).parents("tr").remove();
+    $target = $('.panel-body').append($t);
+      });
+  });
