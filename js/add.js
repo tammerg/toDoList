f3f69b3adc2ed9177d152506f732bad2bd388c50
Tammer Galal <tg385@scarletmail.rutgers.edu>
2015-11-23 23:42:05 -0500

    fixed some things

--- js/add.js
+++ js/add.js
@@ -4,24 +4,13 @@ $(document).ready(function (){
 
     var newWord, newRow, wordTd, newCheck, deleteButton, deleteTd;
     var isDuplicate;
-
     newWord = $(".newWord").val();
     newTime = $(".newTime").val(); 
-    newCheck = $(".newCheck").val(); 
-
-    $("td.word-td").each(function(){
-      if($(this).text() === newWord) {
-        isDuplicate = true;
-        return;
-      }
-    });
-
+    newCheck = $(".newCheck").val();
     var newRow = $("<tr>");
     var newCheck = $("<input>").attr("type", "checkbox").attr("class", "newCheck").attr("data-state", "not-checked");
     var wordTd = $("<td>").append(newWord).before();
     var timeTd = $("<td>").append(newTime).before();
-    
-
     var deleteButton = $("<button>").addClass("btn btn-danger buttonRemove").append("Remove");
     var deleteTd = $("<td>").append(deleteButton);
 
@@ -31,4 +20,12 @@ $(document).ready(function (){
     $("#newWord").val("")
     });
   });
+  $(document).on("click", ".newCheck", function(){
+  if($(this).prop("checked") === true){
+    $(this).parent().attr("class", "done");
+  }
+  else{
+    $(this).parent().removeClass();
+  }
+});
 
